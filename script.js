a3117f1def3ea48bdf11143e55e6be3cd1087765
Ben LAbbe <ben.labbe.224j@gmail.com>
2022-02-04 06:24:15 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-
 function compute()
 {
-    p = document.getElementById("principal").value;
-   
-}
+    var principal = document.getElementById("principal").value;
 
+    if(principal == "" || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
 
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
 
-
-
-
-var principal = document.getElementById("principal").value;     
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
-
-
-
-object.onchange = function()
-{
-    range
-}
-
-function updateRate()
-{
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
     
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-    
+    var resultDisplay = document.getElementById("result");
+    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
 
-document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+
-"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>" 
+function SliderValue()
+{
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate_display");
+    output.innerHTML = slider.value; 
+    
+    slider.oninput = function() 
+    {
+        output.innerHTML = this.value;
+    }  
+}
